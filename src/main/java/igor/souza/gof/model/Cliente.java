2da72d3768f723b2824111eabbe1905798069cb7
igusouz <igorrafael.jobs@gmail.com>
2024-05-19 21:17:16 -0300

    fist commit com projeto

---
+++ src/main/java/igor/souza/gof/model/Cliente.java
@@ -0,0 +1,56 @@
+package igor.souza.gof.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String email;
+    private String CPF;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/main/java/igor/souza/gof/service/ClienteService.java
@@ -0,0 +1,18 @@
+package igor.souza.gof.service;
+
+import igor.souza.gof.model.Cliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
